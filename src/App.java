e3999da84b7d2174bd051155edcac007fb9e504b
JesseFryer <jesse.fryer10@gmail.com>
2023-02-06 15:26:54 +1000

    piece images, new game button - added newGame function to button (sets the piece field in each Tile then updates its imageIcon. - added Piece class to hold static int values for each piece type

--- src/App.java
+++ src/App.java
@@ -1,10 +1,16 @@
 import utils.Constants;
-
+import utils.Piece;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
-public class App extends JFrame {
+public class App extends JFrame implements ActionListener {
 
     private JPanel chessBoard;
 
@@ -12,7 +18,14 @@ public class App extends JFrame {
 
     private JButton newGame;
 
+    private ArrayList<ImageIcon> whitePieceImages;
+    private ArrayList<ImageIcon> blackPieceImages;
+
+
     public App() {
+        //load image sprites
+        whitePieceImages = loadImages("/whitePieces.png");
+        blackPieceImages = loadImages("/blackPieces.png");
 
         // initialising Window
         setTitle("Chess");
@@ -23,7 +36,7 @@ public class App extends JFrame {
         setResizable(false);
 
         // initialise board
-        chessBoard = new JPanel(new GridLayout(8,8));
+        chessBoard = new JPanel(new GridLayout(8, 8));
         add(chessBoard, BorderLayout.CENTER);
 
         // creating tiles
@@ -31,7 +44,6 @@ public class App extends JFrame {
         for (int row = 1; row < 9; row++) {
             for (int column = 1; column < 9; column++) {
                 Tile tile = new Tile(row, column);
-
                 // set colours of squares
                 if (row % 2 == 0) {
                     if (column % 2 == 0) {
@@ -55,8 +67,57 @@ public class App extends JFrame {
 
         // add menu buttons
         newGame = new JButton("New Game");
+        newGame.addActionListener(this);
         menu.add(newGame);
 
         setVisible(true);
+    }
+
+    private void newGame() {
+        for (Tile tile : tiles) {
+            if (tile.row == 2 || tile.row == 7) tile.setPiece(Piece.PAWN);
+            else if (tile.row == 1 || tile.row == 8) {
+                if (tile.column == 1 || tile.column == 8) tile.setPiece(Piece.ROOK);
+                if (tile.column == 2 || tile.column == 7) tile.setPiece(Piece.KNIGHT);
+                if (tile.column == 3 || tile.column == 6) tile.setPiece(Piece.BISHOP);
+                if (tile.column == 4) tile.setPiece(Piece.QUEEN);
+                if (tile.column == 5) tile.setPiece(Piece.KING);
+            } else tile.setPiece(Piece.EMPTY);
+        }
+        updateImages();
+    }
+
+    private void updateImages() {
+        for (Tile tile : tiles) {
+            if (tile.getPiece() != Piece.EMPTY) tile.setIcon(whitePieceImages.get(tile.getPiece()));
+        }
+        revalidate();
+    }
+
+    private ArrayList<ImageIcon> loadImages(String path) {
+        ArrayList<ImageIcon> imageIcons = new ArrayList<>();
+        InputStream is = getClass().getResourceAsStream(path);
+        try {
+            BufferedImage spriteSheet = ImageIO.read(is);
+            for (int x = 0; x < 96; x += 16) {
+                ImageIcon img = new ImageIcon(spriteSheet.getSubimage(x, 0, 16,16));
+                Image scaledImg = img.getImage().getScaledInstance(50,50, Image.SCALE_DEFAULT);
+                imageIcons.add(new ImageIcon(scaledImg));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return imageIcons;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == newGame) newGame();
     }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -5,10 +5,20 @@ public class Tile extends JButton {
     public final int row;
     public final int column;
 
+    private int piece;
+
     public Tile(int row, int column) {
         this.row = row;
         this.column = column;
         setOpaque(true);
         setBorderPainted(false);
+    }
+
+    public void setPiece(int piece) {
+        this.piece = piece;
+    }
+
+    public int getPiece() {
+        return piece;
     }
 }
